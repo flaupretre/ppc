0938e112bd31f96b9b32e75884b49331e55e8ac1
Francois Laupretre <francois.laupretre@externe.bnpparibas.com>
2014-03-13 11:47:33 +0100

    Fix MD char escaping
    Filter code line by line, instead of globally.
    Also fix list lines starting with a tab.

--- lib/functions.php
+++ lib/functions.php
@@ -32,21 +32,32 @@ return str_replace("\n",'</p><p>',htmlspecialchars($str));
 }
 
 //------- MARKDOWN ---------
-// Code blocks are ouput as-is.
+// Lines containing code are ouput as-is.
 // On other text, '_', '*', and '<' chars are translated. To display italic
 // or bold text, original text must be surrounded with double modifiers ('__'
 // or '**'.
 
 function mstring($str)
 {
-if ((!starts_with($str,"\t")) && (!starts_with($str,'    ')))
-	{ // Not a code block
-	$str=str_replace('_','\_',$str);
-	$str=str_replace('\_\_','_',$str);
-	$str=str_replace('*','\*',$str);
-	$str=str_replace('\*\*','**',$str);
-	$str=str_replace('<','&lt;',$str);
-	}
+if ($str==='') return '';
 
-return $str;
+// A '-' may be preceeded by tabs for better readability in the source filecode.
+// It shouldn't be considered as a code line.
+
+$str=preg_replace('/^\t+-/m','-',$str);
+
+$res=array();
+foreach(explode("\n",$str) as $line)
+	{
+	if ((!starts_with($line,"\t")) && (!starts_with($line,'    ')))
+		{ // Not a code line
+		$line=str_replace('_','\_',$line);
+		$line=str_replace('\_\_','_',$line);
+		$line=str_replace('*','\*',$line);
+		$line=str_replace('\*\*','**',$line);
+		$line=str_replace('<','&lt;',$line);
+		}
+	$res[]=$line;
+	}
+return implode("\n",$res);
 }
